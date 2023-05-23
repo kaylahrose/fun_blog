a8f15b196ca608cc5a16ba11cdc31943fb24a5b1
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-05-23 08:24:55 -0600

    Define model relationships and create seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  include BCrypt
+  has_secure_password
+  has_many :comments
+  has_many :statuses
+  has_many :events
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create!(email: 'email@email.com', username: 'wow', password: '123456789012', password_confirmation: '123456789012')
+user2 = User.create!(email: 'email2@email.com', username: 'wow2', password: '123456789012', password_confirmation: '123456789012')
+user.statuses.create!(content: 'wow')
+event = user.events.create!(title: 'party')
+user.comments.create!(body: 'sounds fun', commentable: event)
+user2.comments.create!(body: 'sounds great', commentable: event)
